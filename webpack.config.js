fde4f54d8921a69f5e018ada6035aee8e6099c12
Annalee Engsted <annaleeengsted@Annalees-MBP.lan>
2020-06-04 11:41:55 +0200

    whoopsie found 1 broken link. Now finished version.

--- webpack.config.js
+++ webpack.config.js
@@ -1,58 +1,5 @@
 const path = require("path");
 
-// module.exports = {
-//   entry: path.join(__dirname, "/IndexApp.ts"),
-//   output: {
-//     filename: "IndexApp.js",
-//     path: path.resolve(__dirname, "dist"),
-//   },
-//   module: {
-//     rules: [
-//       {
-//         test: /\.tsx?$/,
-//         loader: "ts-loader",
-//         exclude: /node_modules/,
-//       },
-//     ],
-//   },
-//   resolve: {
-//     extensions: [".tsx", ".ts", ".js"],
-//   },
-//   performance: {
-//     hints: false,
-//     maxEntrypointSize: 512000,
-//     maxAssetSize: 512000,
-//   },
-// };
-
-// module.exports = {
-//   entry: path.join(__dirname, "/app.ts"),
-//   output: {
-//     filename: "app.js",
-//     path: __dirname,
-//   },
-//   module: {
-//     rules: [
-//       {
-//         test: /\.tsx?$/,
-//         loader: "ts-loader",
-//         exclude: /node_modules/,
-//       },
-//       {
-//         test: /\.(s*)css$/,
-//         use: ["style-loader", "css-loader", "sass-loader"],
-//       },
-//       {
-//         test: /\.(png|svg|jpg|gif)$/,
-//         use: ["file-loader"],
-//       },
-//     ],
-//   },
-//   resolve: {
-//     extensions: [".tsx", ".ts", ".js"],
-//   },
-// };
-
 module.exports = {
   entry: {
     base: "./app.ts",
@@ -70,7 +17,7 @@ module.exports = {
     path: __dirname + "/dist",
     filename: "[name].bundle.js",
   },
-  mode: "development",
+  mode: "production",
   module: {
     rules: [
       {
